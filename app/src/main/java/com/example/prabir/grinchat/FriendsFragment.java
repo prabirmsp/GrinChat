9567698340ea92a24dd1433790ce7aabe4bb0064
Prabir Pradhan <prabirmsp@gmail.com>
2015-01-07 01:32:05 -0500

    fragments implemented

---
+++ app/src/main/java/com/example/prabir/grinchat/FriendsFragment.java
@@ -0,0 +1,19 @@
+package com.example.prabir.grinchat;
+
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by prabir on 1/7/15.
+ */
+public class FriendsFragment extends ListFragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/example/prabir/grinchat/SectionsPagerAdapter.java
@@ -0,0 +1,7 @@
+package com.example.prabir.grinchat;
+
+/**
+ * Created by prabir on 1/7/15.
+ */
+public class SectionsPagerAdapter {
+}
