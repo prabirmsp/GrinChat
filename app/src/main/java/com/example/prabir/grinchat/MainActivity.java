395c685409b4fd794f893bcb9777f1f1ff8528f1
prabirmsp <prabirmsp@gmail.com>
2015-01-08 02:06:55 -0500

    Before adding file to message

--- app/src/main/java/com/example/prabir/grinchat/MainActivity.java
+++ app/src/main/java/com/example/prabir/grinchat/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.prabir.grinchat;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
@@ -10,12 +15,35 @@ import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.parse.ParseAnalytics;
 import com.parse.ParseUser;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 
 public class MainActivity extends ActionBarActivity implements ActionBar.TabListener {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
+
+    public static final int TAKE_PHOTO_REQUEST = 0;
+    public static final int TAKE_VIDEO_REQUEST = 1;
+    public static final int CHOOSE_PHOTO_REQUEST = 2;
+    public static final int CHOOSE_VIDEO_REQUEST = 3;
+
+    public static final int MEDIA_TYPE_IMAGE = 4;
+    public static final int MEDIA_TYPE_VIDEO = 5;
+
+    public static final int FILE_SIZE_LIMIT = 10*1024*1024; // 10 MB
+
+    protected Uri mMediaUri;
 
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
@@ -32,7 +60,99 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
      */
     ViewPager mViewPager;
 
-    public static final String TAG = MainActivity.class.getSimpleName();
+    // Take Picture
+    protected DialogInterface.OnClickListener mDialogListener =
+            new DialogInterface.OnClickListener() {
+
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    switch (which) {
+                        case 0: // Take pic
+                            Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                            mMediaUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
+                            if (mMediaUri == null) {
+                                // error
+                                Toast.makeText(MainActivity.this, R.string.error_access_memory, Toast.LENGTH_LONG).show();
+                            }
+                            else {
+                                takePhotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, mMediaUri);
+                                startActivityForResult(takePhotoIntent, TAKE_PHOTO_REQUEST);
+                            }
+                            break;
+                        case 1: // Take vid
+                            Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+                            mMediaUri = getOutputMediaFileUri(MEDIA_TYPE_VIDEO);
+                            if (mMediaUri == null) {
+                                // error
+                                Toast.makeText(MainActivity.this, R.string.error_access_memory, Toast.LENGTH_LONG).show();
+                            }
+                            else {
+                                takeVideoIntent.putExtra(MediaStore.EXTRA_OUTPUT, mMediaUri);
+                                takeVideoIntent.putExtra(MediaStore.EXTRA_DURATION_LIMIT, 10);
+                                takeVideoIntent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 0);
+                                startActivityForResult(takeVideoIntent, TAKE_VIDEO_REQUEST);
+                            }
+                            break;
+                        case 2: // Choose pic
+                            Intent choosePhotoIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                            choosePhotoIntent.setType("image/*");
+                            startActivityForResult(choosePhotoIntent, CHOOSE_PHOTO_REQUEST);
+                            break;
+                        case 3: // Choose vid
+                            Intent chooseVideoIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                            chooseVideoIntent.setType("video/*");
+                            Toast.makeText(MainActivity.this, R.string.video_size_warning, Toast.LENGTH_LONG).show();
+                            startActivityForResult(chooseVideoIntent, CHOOSE_VIDEO_REQUEST);
+                            break;
+
+                    }
+                }
+
+                private Uri getOutputMediaFileUri(int mediaType) {
+                    if (isExternalStorageAvaliable()) {
+                        // get the URI
+
+                        String appName = getString(R.string.app_name);
+                        //get ext storage directory
+                        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), appName);
+                        if (! mediaStorageDir.exists()) {
+                            if (! mediaStorageDir.mkdir()) {
+                                Log.e(TAG, "Failed to create directory.");
+                                return null;
+                            }
+                        }
+
+                        // Create the file
+                        File mediaFile;
+                        Date now = new Date();
+                        String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(now);
+                        String path = mediaStorageDir.getPath() + File.separator;
+                        if (mediaType == MEDIA_TYPE_IMAGE) {
+                            mediaFile = new File( path + "IMG_" + timestamp + ".jpg");
+                            Log.d(TAG, Uri.fromFile(mediaFile).toString());
+                        }
+                        else if (mediaType == MEDIA_TYPE_VIDEO) {
+                            mediaFile = new File( path + "VID_" + timestamp + ".mp4");
+                            Log.d(TAG, Uri.fromFile(mediaFile).toString());
+                        }
+                        else return null;
+
+                        // Return the file's URI
+                        return Uri.fromFile(mediaFile);
+                    }
+                    else return null;
+
+                }
+
+                private boolean isExternalStorageAvaliable() {
+                    String state = Environment.getExternalStorageState();
+
+                    if (state.equals(Environment.MEDIA_MOUNTED)){
+                        return true;
+                    }
+                    else return false;
+                }
+            };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -85,6 +205,81 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
         }
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+
+            if (requestCode == CHOOSE_PHOTO_REQUEST || requestCode == CHOOSE_VIDEO_REQUEST) {
+                if (data == null) {
+                    // Error
+                    toastGenericError();
+                }
+                else {
+                    mMediaUri = data.getData();
+                }
+
+                if (requestCode == CHOOSE_VIDEO_REQUEST) {
+                    // check for under 10 MB
+                    int fileSize = 0;
+                    InputStream inputStream = null;
+                    try {
+                        inputStream = getContentResolver().openInputStream(mMediaUri);
+                        fileSize = inputStream.available();
+                    }
+                    catch (FileNotFoundException e) {
+                        toastGenericError();
+                        return;
+                    }
+                    catch (IOException e) {
+                        toastGenericError();
+                        return;
+                    }
+                    finally {
+                        try {
+                            inputStream.close();
+                        } catch (IOException e) { /* Intentionally blank */ }
+                    }
+                    if (fileSize >= FILE_SIZE_LIMIT) {
+                        Toast.makeText(this, R.string.file_too_large, Toast.LENGTH_LONG).show();
+                        return;
+                    }
+                }
+            }
+            else {
+                // add to gallery
+                Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+                mediaScanIntent.setData(mMediaUri);
+                sendBroadcast(mediaScanIntent);
+            }
+
+            Intent recipientIntent = new Intent(this, RecipientsActivity.class);
+            recipientIntent.setData(mMediaUri);
+
+            String fileType = "";
+            if (requestCode == CHOOSE_PHOTO_REQUEST || requestCode == TAKE_PHOTO_REQUEST) {
+                fileType = ParseConstants.TYPE_IMAGE;
+            }
+            else if (requestCode == CHOOSE_VIDEO_REQUEST || requestCode == TAKE_VIDEO_REQUEST) {
+                fileType = ParseConstants.TYPE_VIDEO;
+            }
+
+            recipientIntent.putExtra(ParseConstants.KEY_FILE_TYPE, fileType);
+            startActivity(recipientIntent);
+
+
+
+        }
+        else if (requestCode != RESULT_CANCELED) {
+            Toast.makeText(this, R.string.signupErrorTitle, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void toastGenericError() {
+        Toast.makeText(MainActivity.this, R.string.signupErrorTitle, Toast.LENGTH_LONG).show();
+    }
+
     private void navigateToLogin() {
         Intent intent = new Intent(this, LoginActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
@@ -108,13 +303,21 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_logout) {
-            ParseUser.logOut();
-            navigateToLogin();
-        }
-        else if (id == R.id.menu_editFriends) {
-            Intent intent = new Intent(this, EditFriendsListActivity.class);
-            startActivity(intent);
+        switch (id) {
+            case R.id.action_logout:
+                ParseUser.logOut();
+                navigateToLogin();
+                break;
+            case R.id.menu_editFriends:
+                Intent intent = new Intent(this, EditFriendsListActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.action_camera:
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setItems(R.array.camera_choices, mDialogListener);
+                AlertDialog dialog = builder.create();
+                dialog.show();
+                break;
         }
 
         return super.onOptionsItemSelected(item);
---
+++ app/src/main/java/com/example/prabir/grinchat/RecipientsActivity.java
@@ -0,0 +1,153 @@
+package com.example.prabir.grinchat;
+
+import android.app.AlertDialog;
+import android.net.Uri;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.parse.FindCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseRelation;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class RecipientsActivity extends ActionBarActivity {
+
+    public static final String TAG = RecipientsActivity.class.getSimpleName();
+
+    protected List<ParseUser> mFriends;
+    protected ParseRelation<ParseUser> mFriendsRelation;
+    protected ParseUser mCurrentUser;
+    protected ListView mListView;
+    protected TextView mNoFriendsTextView;
+    protected Uri mMediaUri;
+    protected String mFileType;
+
+    protected MenuItem mSendMenuItem;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipients);
+
+        mMediaUri = getIntent().getData();
+        mFileType = getIntent().getExtras().getString(ParseConstants.KEY_FILE_TYPE);
+
+        mListView = (ListView)findViewById(R.id.listView);
+        mListView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (0 < mListView.getCheckedItemCount()){
+                    mSendMenuItem.setVisible(true);
+                }
+                else mSendMenuItem.setVisible(false);
+            }
+        });
+        mNoFriendsTextView = (TextView)findViewById(R.id.noFriends);
+
+        mCurrentUser = ParseUser.getCurrentUser();
+        mFriendsRelation = mCurrentUser.getRelation(ParseConstants.KEY_FRIENDS_RELATION);
+
+        ParseQuery<ParseUser> query = mFriendsRelation.getQuery();
+        query.addAscendingOrder(ParseConstants.KEY_USERNAME);
+        query.findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> friends, ParseException e) {
+                if (e == null) {
+                    mFriends = friends;
+
+                    String[] usernames = new String[mFriends.size()];
+                    int i = 0;
+                    for (ParseUser user : mFriends) {
+                        usernames[i] = user.getUsername();
+                        i++;
+                    }
+                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                            mListView.getContext(),
+                            android.R.layout.simple_list_item_checked,
+                            usernames);
+                    mListView.setAdapter(adapter);
+                    if (mFriends.size() == 0) {
+                        mNoFriendsTextView.setVisibility(View.VISIBLE);
+                    }
+                    else mNoFriendsTextView.setVisibility(View.INVISIBLE);
+                }
+                else {
+                    // failure to get friends
+                    Log.e(TAG, e.getMessage());
+                    AlertDialog.Builder builder = new AlertDialog.Builder(mListView.getContext());
+                    builder.setMessage(e.getMessage())
+                            .setTitle(R.string.signupErrorTitle)
+                            .setPositiveButton(android.R.string.ok, null);
+                    AlertDialog alertDialog = builder.create();
+                    alertDialog.show();
+                }
+            }
+        });
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_recipients, menu);
+        mSendMenuItem = menu.getItem(0);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_send) {
+            ParseObject message = createMessege();
+            //send(message);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    protected ParseObject createMessege() {
+        ParseObject message = new ParseObject(ParseConstants.CLASS_MESSAGES);
+        message.put(ParseConstants.KEY_SENDER_ID, ParseUser.getCurrentUser().getObjectId());
+        message.put(ParseConstants.KEY_SENDER_NAME, ParseUser.getCurrentUser().getUsername());
+        message.put(ParseConstants.KEY_RECIPIENT_IDS, getRecipientIds());
+        message.put(ParseConstants.KEY_FILE_TYPE, mFileType);
+
+        byte[] fileBytes;
+
+        return message;
+    }
+
+    protected ArrayList<String> getRecipientIds() {
+        ArrayList<String> recipientIds = new ArrayList<String>();
+        for (int i = 0; i < mListView.getCount(); i++) {
+            if (mListView.isItemChecked(i)) {
+                recipientIds.add(mFriends.get(i).getObjectId());
+            }
+        }
+        return recipientIds;
+    }
+
+}
