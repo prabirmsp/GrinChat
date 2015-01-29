492326f3807e0592fda58d47f40033d7d1fc1536
prabir <prabirmsp@gmail.com>
2015-01-29 01:38:49 -0600

    login graphics implemented

--- app/src/main/java/com/example/prabir/grinchat/MainActivity.java
+++ app/src/main/java/com/example/prabir/grinchat/MainActivity.java
@@ -15,14 +15,9 @@ import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
-import com.parse.FindCallback;
 import com.parse.ParseAnalytics;
-import com.parse.ParseException;
-import com.parse.ParseObject;
-import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
 import java.io.File;
@@ -31,7 +26,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.List;
 import java.util.Locale;
 
 
